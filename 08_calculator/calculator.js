8af0edc041a1825b7ea191b1f617f39ca34afbd8
MVIVN <mvngwerume@gmail.com>
2022-08-23 22:10:05 +1200

    Add reduce functions to perform calculations on running total in order to chain operations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let array = [];
+  if (number === 0) {
+    return 1;
+  } else {
+    for (i = number; i > 0; i--) {
+      array.push(i);
+    };
+    return array.reduce((prevNum, currentNum) => prevNum * currentNum, 1);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+let string = 'rock the microphone! i am done, my boy. you ain\'t got what it takes!'
+const array = string.split(/[\s,.!]+/);
+console.log(array);
